f52dc0c56f199849b44597d5eab4aef4287e2415
evelynea <evelyneujeneza.com>
2022-01-06 15:17:04 +0200

    user's changes

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -4,5 +4,10 @@ import userController from "../controllers/userController";
 const userRoute= express.Router();
     
 userRoute.post("/register", userController.createUser)
+userRoute.get("/all", userController.getAllUsers)
+userRoute.get("/:id", userController.getOneUser)
+userRoute.delete("/:id",userController.deleteOneUser)
+
+
     
 export default userRoute;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,6 +13,41 @@ class userController{
         return res.status(200).json({message:"user created successfully", data:user})
 
     }
+    //getting all registered users
+    static async getAllUsers(req,res){
+        const user = await userInfos.find();
+        if (!user){
+            return res.status(404).json({error:"no users available"})
+        }
+        return res.status(200).json({message:"users successfully retrieved", data:user})
+
+    }
+    //getting one user
+    static async getOneUser(req,res){
+        const user = await userInfos.findById(req.params.id)
+        if (!user){
+            return res.status(404).json({ error: "user not retrieved, check ID" })
+        }
+        return res.status(200).json({ message: "User retrieved successfully", data: user });
+    }
+    //deleting a user
+    static async deleteOneUser(req,res){
+        const user = await userInfos.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).json({ error: "user not deleted" })
+        }
+        return res.status(200).json({ message: "User deleted successfully", data: user });
+
+    }
+
+    // static async deleteOneUser(req, res) {
+    //     const user = await UserInfos.findByIdAndDelete(req.params.id);
+    //     if (!user) {
+    //         return res.status(404).json({ error: "user not deleted" })
+    //     }
+    //     return res.status(200).json({ message: "User deleted successfully", data: user });
+    // }
+   
 }
 
 export default userController;
